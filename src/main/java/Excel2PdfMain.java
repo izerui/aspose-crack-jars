d88c7e0243a2b2ea9384bda538a2f87c09818bd2
serv <liuyuhua69@gmail.com>
2022-09-16 18:10:01 +0800

    破解 cell ，不再需要使用 license文件

--- src/main/java/Excel2PdfMain.java
+++ src/main/java/Excel2PdfMain.java
@@ -1,22 +1,44 @@
+import com.aspose.cells.Cells;
 import com.aspose.cells.Workbook;
+import com.aspose.cells.Worksheet;
 
-import java.io.InputStream;
-import java.net.URL;
+import java.io.StringReader;
+import java.util.UUID;
 
 public class Excel2PdfMain {
-    static {
-        try {
-            URL resource = Excel2PdfMain.class.getClassLoader().getResource("com.aspose.cells.lic_2999.xml");
-            com.aspose.cells.License license = new com.aspose.cells.License();
-            license.setLicense(resource.getFile());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    static {
+//        com.aspose.cells.License license = new com.aspose.cells.License();
+//        license.setLicense(new java.io.StringReader("<License>\n" +
+//                "    <Data>\n" +
+//                "        <Products>\n" +
+//                "            <Product>Aspose.Cells for Java</Product>\n" +
+//                "        </Products>\n" +
+//                "        <EditionType>Enterprise</EditionType>\n" +
+//                "        <SubscriptionExpiry>29991231</SubscriptionExpiry>\n" +
+//                "        <LicenseExpiry>29991231</LicenseExpiry>\n" +
+//                "        <SerialNumber>evilrule</SerialNumber>\n" +
+//                "    </Data>\n" +
+//                "    <Signature>evilrule</Signature>\n" +
+//                "</License>"));
+//        System.out.println("cell破解成功!");
+//    }
+
     public static void main(String[] args) {
         try {
-            Workbook wb = new Workbook("files/excel/更新.xlsx");
-            wb.save("files/excel/更新.pdf", com.aspose.cells.SaveFormat.PDF);
+            Workbook wb = new Workbook("files/excel/产品档案导入模版.xlsx");
+            Worksheet worksheet = wb.getWorksheets().get(0);
+            Cells cells = worksheet.getCells();
+//            cells.checkColumn(13);
+//            cells.get(2, 13);
+//            Column column = worksheet.getCells().checkColumn(13);
+            cells.deleteColumn(14);
+            cells.deleteColumn(15);
+            cells.copyColumns(cells, 13, 1, 13, 3);
+            cells.get(2, 13).setValue("属性" + UUID.randomUUID().toString());
+            cells.get(2, 14).setValue("属性" + UUID.randomUUID().toString());
+            cells.get(2, 15).setValue("属性" + UUID.randomUUID().toString());
+            wb.save("files/excel/产品档案导入模版111.xlsx");
+//            wb.save("files/excel/更新.pdf", com.aspose.cells.SaveFormat.PDF);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/crack/CrackExcelMain.java
+++ src/main/java/crack/CrackExcelMain.java
@@ -1,18 +1,57 @@
 package crack;
 
 import javassist.CtClass;
+import javassist.CtConstructor;
 import javassist.CtMethod;
 
 import java.util.ArrayList;
 import java.util.List;
 
+// https://blog.csdn.net/qq_15827013/article/details/98061772
 public class CrackExcelMain {
     public static void main(String[] args) {
         CrackJar crackJar = new CrackJar("aspose-cells-20.12.jar");
         String crackFilePath = crackJar.crack(pool -> {
             try {
+                CtClass crackClass = pool.get("com.aspose.cells.Workbook");
+                // 添加静态代码块
+                CtConstructor initializer = crackClass.getClassInitializer();
+                initializer.setBody("{\n" +
+                        "        com.aspose.cells.License license = new com.aspose.cells.License();\n" +
+                        "        license.setLicense(new java.io.StringReader(\"<License>\\n\" +\n" +
+                        "                \"    <Data>\\n\" +\n" +
+                        "                \"        <Products>\\n\" +\n" +
+                        "                \"            <Product>Aspose.Cells for Java</Product>\\n\" +\n" +
+                        "                \"        </Products>\\n\" +\n" +
+                        "                \"        <EditionType>Enterprise</EditionType>\\n\" +\n" +
+                        "                \"        <SubscriptionExpiry>29991231</SubscriptionExpiry>\\n\" +\n" +
+                        "                \"        <LicenseExpiry>29991231</LicenseExpiry>\\n\" +\n" +
+                        "                \"        <SerialNumber>evilrule</SerialNumber>\\n\" +\n" +
+                        "                \"    </Data>\\n\" +\n" +
+                        "                \"    <Signature>evilrule</Signature>\\n\" +\n" +
+                        "                \"</License>\"));\n" +
+                        "        System.out.println(\"Aspose.Cells for Java 已授权!\");\n" +
+                        "    }");
+
                 List<CtClass> crackClassList = new ArrayList<>();
                 CtClass cc_License = pool.get("com.aspose.cells.License");
+
+//                CtConstructor[] constructors = cc_License.getConstructors();
+//                for (CtConstructor constructor : constructors) {
+//                    constructor.setBody("{this.setLicense(new java.io.StringReader(\"<License>\\n\" +\n" +
+//                            "                \"    <Data>\\n\" +\n" +
+//                            "                \"        <Products>\\n\" +\n" +
+//                            "                \"            <Product>Aspose.Cells for Java</Product>\\n\" +\n" +
+//                            "                \"        </Products>\\n\" +\n" +
+//                            "                \"        <EditionType>Enterprise</EditionType>\\n\" +\n" +
+//                            "                \"        <SubscriptionExpiry>29991231</SubscriptionExpiry>\\n\" +\n" +
+//                            "                \"        <LicenseExpiry>29991231</LicenseExpiry>\\n\" +\n" +
+//                            "                \"        <SerialNumber>evilrule</SerialNumber>\\n\" +\n" +
+//                            "                \"    </Data>\\n\" +\n" +
+//                            "                \"    <Signature>evilrule</Signature>\\n\" +\n" +
+//                            "                \"</License>\"));\n" +
+//                            "        System.out.println(\"Aspose.Cells for Java 破解成功!\");}\n");
+//                }
                 CtMethod method_isLicenseSet = cc_License.getDeclaredMethod("isLicenseSet");
                 method_isLicenseSet.setBody("return true;");
 
@@ -36,6 +75,7 @@ public class CrackExcelMain {
                 method_k.setBody("return true;");
 
                 crackClassList.add(cc_License);
+                crackClassList.add(crackClass);
                 return crackClassList;
             } catch (Exception ex) {
                 throw new RuntimeException(ex);
